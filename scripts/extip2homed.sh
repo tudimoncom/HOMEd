7dbe62ac0bf5152790c283bb347c2dce9bec1442
tudimoncom <81709216+tudimoncom@users.noreply.github.com>
2024-11-30 13:35:07 +0300

    Update extip2homed.sh

--- scripts/extip2homed.sh
+++ scripts/extip2homed.sh
@@ -9,5 +9,5 @@ ext_ip=`curl 2ip.ru`
 echo $ext_ip
 #ext_ip="23"
 
-mosquitto_pub -h localhost -p 1883 -t homed/fd/custom/extip -m "{\"externalip\":\"$ext_ip\"}" -u "usermqtt" -P "***"
+mosquitto_pub -h localhost -p 1883 -t homed/fd/custom/extip -m "{\"externalip\":\"$ext_ip\"}" -u "homed" -P "pass_mqtt_homed"
 
